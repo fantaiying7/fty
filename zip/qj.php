2b0d2b8c431c7cef59e56d51edf26eac5f32741a
饭太硬 <107107882+fantaiying7@users.noreply.github.com>
2022-08-08 00:43:49 +0800

    Update qj.php
    Signed-off-by: 饭太硬 <107107882+fantaiying7@users.noreply.github.com>

--- zip/qj.php
+++ zip/qj.php
@@ -7,7 +7,7 @@ $pattern='#'.preg_quote($start).'(.+?)'.preg_quote($end).'#s';
 preg_match_all($pattern, $temp, $m);
 $playurl0=$m[0][0];
 $qiaoji=str_ireplace('"key": "csp_xpath_huya",', '', $playurl0);
-$temp = file_get_contents('http://a.b.c.d/qj3.json');
+$temp = file_get_contents('https://ftyyy.tk/json/wl.txt');
 $playurl=str_ireplace('qiaoji_jar', $qiaoji, $temp );
 echo $playurl;
 ?>
