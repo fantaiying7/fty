e973220b1878b948c7a948068e4f211b0dd07b5b
饭太硬 <107107882+fantaiying7@users.noreply.github.com>
2022-09-02 15:47:02 +0800

    Add files via upload

---
+++ wallpapers/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$myLinks = array("https://takagen99.github.io/wallpapers/01.png", 
+    "https://takagen99.github.io/wallpapers/02.png",
+    "https://takagen99.github.io/wallpapers/03.jpg",
+    "https://takagen99.github.io/wallpapers/04.jpg",
+    "https://takagen99.github.io/wallpapers/05.jpg",
+    "https://takagen99.github.io/wallpapers/06.jpg",
+    "https://takagen99.github.io/wallpapers/07.png",
+    "https://takagen99.github.io/wallpapers/08.png",
+    "https://takagen99.github.io/wallpapers/09.png",
+    "https://takagen99.github.io/wallpapers/10.png",
+    "https://takagen99.github.io/wallpapers/11.jpg",
+    "https://takagen99.github.io/wallpapers/12.png",
+    "https://takagen99.github.io/wallpapers/13.jpg",
+    "https://takagen99.github.io/wallpapers/14.jpg",
+    "https://takagen99.github.io/wallpapers/15.jpg",
+    "https://takagen99.github.io/wallpapers/16.jpg",
+	"https://takagen99.github.io/wallpapers/17.jpg");
+
+$randomRedirection = $myLinks[array_rand($myLinks)];
+header("Location: $randomRedirection");
+?>
+
+</body>
+</html>
